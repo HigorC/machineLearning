ea9140b65d70909fa0790fb33147c1167b8fe040
Higor Chaves <higor_oliveira@outlook.com>
2020-05-18 23:08:48 -0400

    RNA - Rede multicamada

---
+++ RNA Udemy/rede_multicamada.py
@@ -0,0 +1,45 @@
+import numpy as np
+
+
+def sigmoid(soma):
+    return 1 / (1 + np.exp(-soma))
+
+def sigmoidDerivada(sig):
+    return sig * (1 - sig)
+
+
+entradas = np.array([[0, 0], [0, 1], [1, 0], [1, 1]])
+saidas = np.array([[0], [1], [1], [0]])
+
+pesos0 = np.array([[-0.424, -0.740, -0.961],
+                   [0.358, -0.577, -0.469]])
+
+pesos1 = np.array([[-0.017], [-0.893], [0.148]])
+
+epocas = 100
+
+# for j in range(epocas):
+camadaEntrada = entradas
+somaSinapse0 = np.dot(camadaEntrada, pesos0)
+camadaOculta = sigmoid(somaSinapse0)
+
+somaSinapse1 = np.dot(camadaOculta, pesos1)
+camadaSaida = sigmoid(somaSinapse1)
+# print(somaSinapse0)
+print(camadaOculta)
+print(camadaSaida)
+print(camadaSaida - saidas)
+
+
+# Python faz várias contas de uma vez
+# Aqui está subtraindo cada posição no vetor pelo outro
+erroCamadaSaida = saidas - camadaSaida
+mediaAbsoluta = np.mean(np.abs(erroCamadaSaida))
+
+derivadaSaida = sigmoidDerivada(camadaSaida)
+deltaSaida = erroCamadaSaida * derivadaSaida
+
+pesosTransposta = pesos1.T
+deltaSaidaXPeso = deltaSaida.dot(pesosTransposta)
+deltaCamadaOculta = deltaSaidaXPeso * sigmoidDerivada(camadaOculta)
+print(derivadaSaida)
